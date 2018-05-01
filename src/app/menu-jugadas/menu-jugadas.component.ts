3835fcd09aed1a5c71b449ec09e2db4964c700fc
pclr0120 <pablo.pclr@hotmail.com>
2018-05-01 00:19:42 -0600

    Diseño mejorado Menu de jugadas

--- src/app/menu-jugadas/menu-jugadas.component.ts
+++ src/app/menu-jugadas/menu-jugadas.component.ts
@@ -6,10 +6,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu-jugadas.component.css']
 })
 export class MenuJugadasComponent implements OnInit {
-
+  //Jugadas 
+Jugadas:any[]=["CENTRO","CHORRO","4 ESQUINAS","LLENA"];
+//monedas 
+  Apuesta: any[] = [20,30,10,50];
+  //acumulado
+  acu:any=200;
+  //Verificacion de jugadas 1 para jugada verificada 0 para diponible
+  JugadaV:any[]=[true,false,true,false]
   constructor() { }
+  
+
+  //Apuesta de cada jugada
+
 
   ngOnInit() {
+  }
+
+  jugada1(){
+
+
+  }
+  jugada2(){
+
+    
+  }
+  jugada3(){
+
+    
+  }
+  jugada4(){
+
+    
   }
 
 }
