18b3524a024d2617d541630f07b026285aed67d5
Adrian CP <adriancarreon3@gmail.com>
2018-05-05 20:47:00 -0600

    Lobby

--- src/app/menu-jugadas/menu-jugadas.component.ts
+++ src/app/menu-jugadas/menu-jugadas.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit,EventEmitter,  Output , Input } from '@angular/core';
+import * as io from "socket.io-client";
 
 @Component({
   selector: 'app-menu-jugadas',
   templateUrl: './menu-jugadas.component.html',
   styleUrls: ['./menu-jugadas.component.css']
 })
+
 export class MenuJugadasComponent implements OnInit {
+  socket: SocketIOClient.Socket;
   //Jugadas 
 Jugadas:any[]=["CHORRO","CENTRO","4 ESQUINAS","LLENA"];
 //monedas 
@@ -14,7 +17,21 @@ Jugadas:any[]=["CHORRO","CENTRO","4 ESQUINAS","LLENA"];
   acu:any=200;
   //Verificacion de jugadas 1 para jugada verificada 0 para diponible
   JugadaV:any[]=[false,false,false,false]
-  constructor() { }
+  constructor() {
+    var socket = io.connect('http://localhost:8080', {'forceNew':true});
+
+    var payload={
+      autor:"Carry",
+      text: "Hola",
+      verificar: false
+    };
+
+    socket.emit("adduser","carry","sala1");
+
+    socket.on("jugada", function(data){
+      console.log(data);
+    });
+  }
   
 
 
---
+++ src/app/servicios/socket.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@angular/core';
+
+import { Subject } from 'rxjs/Subject';
+import { Observable } from 'rxjs/Observable';
+import * as io from 'socket.io-client';
+
+@Injectable()
+export class SocketService {
+
+  private url = 'http://localhost:8080';
+  private socket;
+
+  constructor() {
+    this.socket = io(this.url);
+    //EVENTO PARA CONECTARTE AL SOCKET
+    this.socket.on('connect', function(){
+      console.log("Conectado al socket!");
+    });
+   }
+   
+  Desconectar(){
+    this.socket.emit('desconectar');
+  }
+
+  //UNIR USUARIOS A UNA SALA
+  addUser(username, nombreSala){
+    this.socket.emit('adduser', username, nombreSala);
+  }
+
+  //Obtener salas activas
+  getSalas(){
+    let observable = new Observable(observer => {
+      this.socket.emit('Salas');
+      this.socket.on('SalasInf', function(salas){
+        //console.log("socket " + salas);
+        observer.next(salas);
+      });
+
+    });
+    return observable;
+  }
+
+  
+
+}
