2ded8a4bcc2a28ac4ae455b28790ecc3de1444da
Fcrow712 <f.crow712@gmail.com>
2018-05-20 21:49:34 -0600

    Servicio Cash

--- src/app/servicios/registrousuario.service.ts
+++ src/app/servicios/registrousuario.service.ts
@@ -7,34 +7,45 @@ export class RegistrousuarioService {
   constructor(private http:Http) { }
   presURL = 'http://localhost:4000/usuarios';
 
-  
+  getUser(){
+    return this.http.get(this.presURL).map(res => res.json())
+    .toPromise()
+    .then(data => { return data; });
+  }
 
-  
-getUser(){
-  return this.http.get(this.presURL).map(res => res.json())
-  .toPromise()
-  .then(data => { return data; });
-}
+  getObtenerNumUser(){
+    return this.http.get('http://localhost:4000/usuariosContar')
+    .map(res => res.json());
+  }
 
-getObtenerNumUser(){
-  return this.http.get('http://localhost:4000/usuariosContar')
-  .map(res => res.json());
-}
-
-postUser(user: any) {
-  const newpres = JSON.stringify(user);
-  console.log(user)
-  const headers = new Headers({ 'Content-Type': 'Application/json' });
-  return this.http.post(this.presURL, newpres, { headers })
-
+  updateCash(idUsuario: any, cash: any) {
+    var URL = 'http://localhost:4000/updatecash';
+    const newpres = {
+      "idUsuario": idUsuario,
+      "cash": cash
+    }
+    const headers = new Headers({ 'Content-Type': 'Application/json' });
+    return this.http.put(URL, newpres, { headers })
     .map(res => {
-
       console.log(res.json());
       return res.json();
-
     })
+  }
 
-}
+  postUser(user: any) {
+    const newpres = JSON.stringify(user);
+    console.log(user)
+    const headers = new Headers({ 'Content-Type': 'Application/json' });
+    return this.http.post(this.presURL, newpres, { headers })
+
+      .map(res => {
+
+        console.log(res.json());
+        return res.json();
+
+      })
+
+  }
 
 }
 
